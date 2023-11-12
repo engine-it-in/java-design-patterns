2bf730ee7f87d1c0caadeecb5e2b0b564ac7939a
engine-it-in <engineit@mail.ru>
2023-11-12 23:37:08 +0300

    Add TemplateMethod and delete old unit test

--- src/test/java/factorymethod/logic/collect/SupplierTest.java
+++
@@ -1,8 +0,0 @@
-package factorymethod.logic.collect;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-public class SupplierTest {
-}
