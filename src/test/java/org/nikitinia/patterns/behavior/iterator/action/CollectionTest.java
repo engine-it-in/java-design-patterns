8a098101c6753cd398e501335576aef7daf95310
engine-it-in <engineit@mail.ru>
2023-12-03 23:45:15 +0300

    * Add Iterator logic

---
+++ src/test/java/org/nikitinia/patterns/behavior/iterator/action/CollectionTest.java
@@ -0,0 +1,10 @@
+package org.nikitinia.patterns.behavior.iterator.action;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CollectionTest {
+
+
+}
