c76a7330de151090da4cf9bda609e1682c69f437
engine-it-in <engineit@mail.ru>
2023-12-12 12:29:13 +0300

    * Refactor. Add main logic and description.

---
+++ src/main/java/org/nikitinia/patterns/behavior/mediator/util/Constant.java
@@ -0,0 +1,10 @@
+package org.nikitinia.patterns.behavior.mediator.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constant {
+
+    public static final String TEST_NAME = "name";
+
+}
--- src/test/java/org/nikitinia/patterns/behavior/mediator/actor/OperatorTest.java
+++ src/test/java/org/nikitinia/patterns/behavior/mediator/actor/OperatorTest.java
@@ -1,7 +1,15 @@
 package org.nikitinia.patterns.behavior.mediator.actor;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.nikitinia.patterns.behavior.mediator.logic.AudioDocumentSystem;
+
+import static org.nikitinia.patterns.behavior.mediator.util.Constant.TEST_NAME;
 
 class OperatorTest {
+
+    private final AudioDocumentSystem audioDocumentSystem = new AudioDocumentSystem();
+
+    private final Operator operator = new Operator(audioDocumentSystem, TEST_NAME);
+
+
 
 }
