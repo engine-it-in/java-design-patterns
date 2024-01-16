5b4a8a28016b81b9735b9a64dcf5141db11647d7
engine-it-in <engineit@mail.ru>
2024-01-17 00:09:36 +0300

    * Add adapter unit test

--- src/test/java/org/nikitinia/patterns/structure/adapter/actor/DocumentRepositoryTest.java
+++ src/test/java/org/nikitinia/patterns/structure/adapter/actor/DocumentRepositoryTest.java
@@ -3,6 +3,9 @@ package org.nikitinia.patterns.structure.adapter.actor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.nikitinia.domain.creator.DocumentCreator;
 import org.nikitinia.domain.model.documents.Document;
 
@@ -11,8 +14,12 @@ import java.io.PrintStream;
 import java.util.HashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
 
 @Slf4j
+@ExtendWith(MockitoExtension.class)
 class DocumentRepositoryTest {
 
     private final DocumentRepository documentRepository
@@ -24,8 +31,12 @@ class DocumentRepositoryTest {
     private final ByteArrayOutputStream outputStream
             = new ByteArrayOutputStream();
 
+    @Mock
+    private DocumentRepository documentRepositoryMock;
+
     @BeforeEach
     void setUp() {
+
         System.setOut(new PrintStream(outputStream));
     }
 
@@ -46,6 +57,45 @@ class DocumentRepositoryTest {
         assertThat(documentRepository.getDocumentMap().get(document.getNumber()))
                 .usingRecursiveComparison()
                 .isEqualTo(document);
+    }
+
+    @Test
+    void load_shouldReturnResult() {
+
+        documentRepository.save(document);
+
+        assertThat(documentRepository.load(document.getNumber()))
+                .usingRecursiveComparison()
+                .isEqualTo(document);
+
+        assertThat(outputStream.toString())
+                .contains("Load");
+
+        assertThat(documentRepository.getDocumentMap())
+                .hasSize(1);
+    }
+
+    @Test
+    void delete_shouldDo() {
+        doAnswer(invocation -> {
+            Document arg = invocation.getArgument(0);
+            assertEquals(document, arg);
+            return null;
+        }).when(documentRepositoryMock).delete(any(Document.class));
+
+        documentRepositoryMock.delete(document);
+    }
+
+    @Test
+    void delete_shouldLogAndDelete() {
+        documentRepository.save(document);
+        documentRepository.delete(document);
+
+        assertThat(outputStream.toString())
+                .contains("Delete");
+
+        assertThat(documentRepository.getDocumentMap())
+                .hasSize(0);
     }
 
 }
---
+++ src/test/java/org/nikitinia/patterns/structure/adapter/logic/AdapterTest.java
@@ -0,0 +1,113 @@
+package org.nikitinia.patterns.structure.adapter.logic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.nikitinia.domain.creator.DocumentCreator;
+import org.nikitinia.domain.dictionarys.Status;
+import org.nikitinia.domain.model.documents.Document;
+import org.nikitinia.patterns.structure.adapter.action.DocumentDatabase;
+import org.nikitinia.patterns.structure.adapter.actor.DocumentRepository;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+class AdapterTest {
+
+    private final Adapter adapter = new Adapter();
+
+    private final ByteArrayOutputStream outputStream =
+            new ByteArrayOutputStream();
+
+    private final Document document =
+            DocumentCreator.documentBuildWithNumber(1.0);
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @Test
+    void checkAdapter() {
+        assertThat(adapter)
+                .isInstanceOf(DocumentRepository.class)
+                .isInstanceOf(DocumentDatabase.class);
+    }
+
+    @Test
+    void insert_shouldDo() {
+        adapter.insert(document);
+
+        assertThat(outputStream.toString())
+                .contains("Save");
+
+        assertThat(adapter.getDocumentMap().get(document.getNumber()))
+                .usingRecursiveComparison()
+                .isEqualTo(document);
+    }
+
+    @Test
+    void select_shouldDo() {
+        adapter.insert(document);
+
+        assertThat(adapter.select(document.getNumber()))
+                .usingRecursiveComparison()
+                .isEqualTo(document);
+
+        assertThat(outputStream.toString())
+                .contains("Load");
+
+    }
+
+    @Test
+    void update_shouldDo() {
+        adapter.insert(document);
+        adapter.update(document);
+
+        assertThat(adapter.getDocumentMap().get(document.getNumber()))
+                .isInstanceOfSatisfying(Document.class, documentData ->
+                        assertThat(documentData.getStatus()).isEqualTo(Status.SIGN));
+
+        assertThat(adapter.getDocumentMap())
+                .hasSize(1);
+
+        assertThat(outputStream.toString())
+                .contains("Load")
+                .contains("Delete")
+                .contains("Save");
+    }
+
+    @Test
+    void remove_shouldDo() {
+        adapter.insert(document);
+        adapter.remove(document);
+
+        assertThat(outputStream.toString())
+                .contains("Delete");
+
+        assertThat(adapter.getDocumentMap())
+                .hasSize(0);
+    }
+
+    @Test
+    void stateDocumentMap_shouldNotEmpty() {
+        adapter.insert(document);
+        adapter.stateDocumentMap();
+
+        assertThat(outputStream.toString())
+                .contains(String.format("documentMap contain document with number: %s", document.getNumber()));
+
+    }
+
+    @Test
+    void stateDocumentMap_shouldEmpty() {
+        adapter.stateDocumentMap();
+
+        assertThat(outputStream.toString())
+                .contains("DocumentMap is Empty");
+    }
+
+}
