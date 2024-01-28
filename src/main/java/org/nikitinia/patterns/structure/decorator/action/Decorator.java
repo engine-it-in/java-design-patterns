651874219dbafb3280703ee5b77ee8b297fc4784
engine-it-in <engineit@mail.ru>
2024-01-28 23:00:38 +0300

    * Add doc for decorator

--- src/main/java/org/nikitinia/patterns/structure/decorator/action/Decorator.java
+++ src/main/java/org/nikitinia/patterns/structure/decorator/action/Decorator.java
@@ -3,6 +3,12 @@ package org.nikitinia.patterns.structure.decorator.action;
 import lombok.AllArgsConstructor;
 import org.nikitinia.patterns.structure.decorator.actor.SecurityDocument;
 
+/*
+ * Что -> Класс, реализующий ядро декоратора. Класс имплементирует интерфейс, декорирующий метод, дополнение которого должно входить в ответсвенность декоратора;
+ * Для чего -> Определение базового поведения декорирующих объектов;
+ * Реализация -> Класс, исплементирующий контракт декорации;
+ * В чем выгода -> Возможность наследования класса декорирующими потомками;
+ * */
 @AllArgsConstructor
 public class Decorator implements Sender {
 
