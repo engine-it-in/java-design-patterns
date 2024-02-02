8431aed5f5392162b702d9b3957435a7051a778c
engine-it-in <engineit@mail.ru>
2024-02-02 19:41:25 +0300

    * Add readme

--- src/main/java/org/nikitinia/patterns/structure/facade/dictionary/TypeMobile.java
+++ src/main/java/org/nikitinia/patterns/structure/facade/dictionary/TypeMobile.java
@@ -4,16 +4,20 @@ import lombok.AllArgsConstructor;
 
 import java.util.Random;
 
+/*
+ * Что -> ;
+ * Для чего -> ;
+ * Реализация -> ;
+ * В чем выгода -> ;
+ * */
 @AllArgsConstructor
 public enum TypeMobile {
 
-    ANDROID("ANDROID"),
+    ANDROID,
 
-    IOS("IOS"),
+    IOS,
 
-    WINDOWS("WINDOWS");
-
-    private final String typeMobile;
+    WINDOWS;
 
     public static TypeMobile randomType() {
         TypeMobile[] typeMobiles = values();
