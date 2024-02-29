3041b6e6657b246c94e5dd249505ddfd6025dd5d
engine-it-in <engineit@mail.ru>
2024-03-01 00:11:28 +0300

    * Add proxy logic

--- src/main/java/org/nikitinia/exception/BusinessException.java
+++ src/main/java/org/nikitinia/exception/BusinessException.java
@@ -1,5 +1,11 @@
 package org.nikitinia.exception;
 
+/*
+ * Что -> Класс обработчик исключительных ситуаций;
+ * Для чего -> Для обработки возможных проблем в коде;
+ * Реализация -> Класс, наследуемый от общего класса ошибки, возникающей во время исполнения программы;
+ * В чем выгода -> Обработка исключительных ситуаций;
+ * */
 public class BusinessException extends RuntimeException {
     public BusinessException(String message) {
         super(message);
