e497c7cac2d9db333da71d357d556e822658c250
engine-it-in <engineit@mail.ru>
2024-03-13 00:35:53 +0300

    * Add test to factory method

---
+++ src/test/java/org/nikitinia/patterns/creating/factorymethod/action/MediaDocumentCreatorTest.java
@@ -0,0 +1,26 @@
+package org.nikitinia.patterns.creating.factorymethod.action;
+
+import org.junit.jupiter.api.Test;
+import org.nikitinia.patterns.creating.factorymethod.actor.MediaDocument;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MediaDocumentCreatorTest {
+
+    private final MediaDocumentCreator mediaDocumentCreator
+            = new MediaDocumentCreator();
+
+    @Test
+    void checkMediaDocumentCreator() {
+        assertThat(mediaDocumentCreator)
+                .isInstanceOf(DocumentCreator.class);
+    }
+
+    @Test
+    void createDocument_shouldReturnResult() {
+        assertThat(mediaDocumentCreator.createDocument())
+                .usingRecursiveComparison()
+                .isEqualTo(new MediaDocument());
+    }
+
+}
