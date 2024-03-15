1006af6d74f7f8258e961697e2e5bb363e1186bc
engine-it-in <engineit@mail.ru>
2024-03-15 19:20:43 +0300

    * Add description task

--- src/main/java/org/nikitinia/patterns/creating/factorymethod/action/DocumentCreator.java
+++ src/main/java/org/nikitinia/patterns/creating/factorymethod/action/DocumentCreator.java
@@ -2,6 +2,12 @@ package org.nikitinia.patterns.creating.factorymethod.action;
 
 import org.nikitinia.patterns.creating.factorymethod.actor.Document;
 
+/**
+ * Что -> Интерфейс, который регламентирует работу по оповещению;
+ * Для чего -> Общий контракт действий, которые будет реализовывать фабричный метод;
+ * Реализация -> Интерфейс с реализацией по-умолчанию, которую можно будет переопределить;
+ * В чем выгода -> Регламентация требуемого действия;
+ */
 public interface DocumentCreator {
 
     Document createDocument();
--- src/main/java/org/nikitinia/patterns/creating/factorymethod/action/MediaDocumentCreator.java
+++ src/main/java/org/nikitinia/patterns/creating/factorymethod/action/MediaDocumentCreator.java
@@ -3,6 +3,13 @@ package org.nikitinia.patterns.creating.factorymethod.action;
 import org.nikitinia.patterns.creating.factorymethod.actor.MediaDocument;
 import org.nikitinia.patterns.creating.factorymethod.actor.Document;
 
+/**
+ * Что -> Класс, реализующий фабричный интерфейс;
+ * Для чего -> Инкапсуляция оповещения в конкретном классе с общим интерфейсом;
+ * Реализация -> Класс с фабричным интерфейсом;
+ * В чем выгода -> Мы инкапсулируем работу с оповещением и работаем с ним через "производителя".
+ * Так мы оставляем клиентский код максимально не изменяемым;
+ */
 public class MediaDocumentCreator implements DocumentCreator {
     @Override
     public Document createDocument() {
