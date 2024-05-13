ff26071c382b02ba8170349fbc34409b16a1e932
engine-it-in <engineit@mail.ru>
2024-05-13 23:56:56 +0300

    * Refactor creating pattern

--- src/main/java/org/nikitinia/patterns/creating/prototype/action/Copy.java
+++ src/main/java/org/nikitinia/patterns/creating/prototype/action/Copy.java
@@ -3,10 +3,10 @@ package org.nikitinia.patterns.creating.prototype.action;
 import org.nikitinia.patterns.creating.prototype.actor.PatternDocument;
 
 /**
- * Что -> ?;
- * Для чего -> ?;
- * Реализация -> ?;
- * Ценность -> ?;
+ * Что -> Интерфейс, регламентирующий прототипирование;
+ * Для чего -> Выделение основного действия, которое приводит к созданию прототипа;
+ * Реализация -> Интерфейс;
+ * Ценность -> Гибкая регламентация прототипирования;
  */
 public interface Copy {
 
--- src/main/java/org/nikitinia/patterns/creating/prototype/actor/DocumentFactory.java
+++ src/main/java/org/nikitinia/patterns/creating/prototype/actor/DocumentFactory.java
@@ -3,10 +3,10 @@ package org.nikitinia.patterns.creating.prototype.actor;
 import lombok.AllArgsConstructor;
 
 /**
- * Что -> ?;
- * Для чего -> ?;
- * Реализация -> ?;
- * Ценность -> ?;
+ * Что -> Создание копии объекта;
+ * Для чего -> Реализация прототипирования;
+ * Реализация -> Класс с атрибутом объекта, с которого создается прототип;
+ * Ценность -> Агрегация прототипируемого объекта;
  */
 @AllArgsConstructor
 public class DocumentFactory {
